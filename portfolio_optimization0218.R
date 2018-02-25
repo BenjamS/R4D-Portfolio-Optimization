4fefa99f4f6c3895706ef1a329a0ea6d63db595c
BenjamS <ben.schiek@gmail.com>
2018-02-25 14:27:54 -0500

    Update portfolio_optimization0218.R

--- portfolio_optimization0218.R
+++ portfolio_optimization0218.R
@@ -3,15 +3,15 @@ library(gridExtra)
 library(stats)
 library(pracma)
 library(nleqslv)
-source('D:/OneDrive - CGIAR/Documents/EUwCconstr.R')
-source('D:/OneDrive - CGIAR/Documents/coloredNoise.R')
-# source('~/EUwCconstr.R')
-# source('~/coloredNoise.R')
+# source('D:/OneDrive - CGIAR/Documents/minV_EnetUconstr.R')
+# source('D:/OneDrive - CGIAR/Documents/coloredNoise.R')
+source('~/minV_EnetUconstr.R')
+source('~/coloredNoise.R')
 #============================================
 #N risky assets
 #============================================
-m_b <- c(-0.37, -0.55, -0.78, -0.83)
-cv_b <- c(0.65, 0.56, 0.79, 0.43) * sign(m_b)
+m_b <- c(-0.37, -0.45, -0.28, -0.63)
+cv_b <- c(0.45, 0.31, 0.52, 0.23) * sign(m_b)
 s_b <- cv_b * m_b
 m_lA <- c(-.65, -0.75, -0.53, -0.3)
 cv_lA <- c(0.43, 0.81, 0.51, 0.3) * sign(m_lA)
@@ -26,14 +26,13 @@ E_lA <- sum(m_lA)
 noisevec <- coloredNoise(n_proj^2, a = -2, normalize = T, graph = T)
 Q <- matrix(exp(noisevec), n_proj, n_proj)
 Corr <- Q %*% t(Q)
-Corr <- Corr / max(Corr) * .5
+Corr <- Corr / max(Corr) * 0.1
 diag(Corr) <- rep(1, n_proj)
 #print(Corr)
 S_b <- diag(s_b) %*% Corr %*% diag(s_b)
 Sb_inv <- round(solve(S_b), 5)
 #============================================
-C_targ <- 1
-Vb_targ <- 0.6
+Vb_targ <- 0.9
 #--------------------------------------------
 rootfn <- T
 quietly <- F
@@ -47,7 +46,7 @@ Cost <- sum(wstar)
 Cost
 #============================================
 pwr10 <- 2
-range_V <- c(0, 2.5)
+range_V <- c(0, 2)
 first_i <- range_V[1] * 10^pwr10
 last_i <- range_V[2] * 10^pwr10
 #---------------
@@ -55,7 +54,6 @@ EUb_vec <- c()
 Vb_vec <- c()
 MD_vec <- c()
 lNEU_vec <- c()
-#lC_vec <- c()
 C_vec <- c()
 wstar_list <- list()
 slackvec_list <- list()
@@ -66,45 +64,47 @@ for(i in first_i:last_i){
   Vb_targ <- i / 10^pwr10
   #----
   for(j in 1:40){
-  rootfn <- T
-  quietly <- T
-  in_vec <- runif(n_proj + 1)
-  out <- try(nleqslv(in_vec, minV_EnetUconstr, jac = NULL, Vb_targ, m_b, S_b, rootfn, quietly))
-  if(inherits(out, "try-error")){print("gotta skip"); next()}
-  if(max(abs(out$fvec)) > 10^-5){print("max iter with no convergence");next()}
-  #----
-  instar <- out$x
-  #----
-  rootfn <- F
-  quietly <- T
-  outstar <- minV_EnetUconstr(instar, Vb_targ, m_b, S_b, rootfn, quietly)
-  #----
-  EUb_try <- round(outstar[[2]], 5)
-  if(EUb_try %in% EUb_vec){print("already have"); next()}
-  #----
-  t <- t + 1
-  #----
-  wstar <- instar[1:n_proj]
-  l_NEU <- instar[n_proj + 1]
-  #l_C <- instar[n_proj + 2]  
-  slackvec_list[[t]] <- outstar$fvec
-  wstar_list[[t]] <- wstar
-  Vb_vec[t] <- Vb_targ
-  EUb_vec[t] <- EUb_try
-  lEU_vec[t] <- l_EU
-  #lC_vec[t] <- l_C
-  MD_vec[t] <- outstar[[3]]
-  C_vec[t] <- sum(wstar)
-  #---
-  if(sum(wstar < 0) > 0){negpos[t] <- "negative"}else{negpos[t] <- "positive"}
+    rootfn <- T
+    quietly <- T
+    in_vec <- runif(n_proj + 1)
+    out <- try(nleqslv(in_vec, minV_EnetUconstr, jac = NULL, Vb_targ, m_b, S_b, rootfn, quietly))
+    if(inherits(out, "try-error")){print("gotta skip"); next()}
+    if(max(abs(out$fvec)) > 10^-5){print("max iter with no convergence");next()}
+    #----
+    instar <- out$x
+    #----
+    rootfn <- F
+    quietly <- T
+    outstar <- minV_EnetUconstr(instar, Vb_targ, m_b, S_b, rootfn, quietly)
+    #----
+    EUb_try <- round(outstar[[2]], 5)
+    if(EUb_try %in% EUb_vec){print("already have"); next()}
+    #----
+    t <- t + 1
+    #----
+    wstar <- instar[1:n_proj]
+    l_NEU <- instar[n_proj + 1]
+    #l_C <- instar[n_proj + 2]  
+    slackvec_list[[t]] <- outstar$fvec
+    wstar_list[[t]] <- wstar
+    Vb_vec[t] <- Vb_targ
+    EUb_vec[t] <- EUb_try
+    lNEU_vec[t] <- l_NEU
+    #lC_vec[t] <- l_C
+    MD_vec[t] <- outstar[[3]]
+    C_vec[t] <- sum(wstar)
+    #---
+    if(sum(wstar < 0) > 0){negpos[t] <- "negative"}else{negpos[t] <- "positive"}
   }
   
 }
 
 risk_vec <- sqrt(Vb_vec)
+NEUb_vec <- EUb_vec - log(C_vec)
+EUbtest_vec <- -risk_vec * sqrt(MD_vec) + 1
 wstars <- wstar_list
-df <- data.frame(EUb = EUb_vec, Risk = risk_vec, lNEU = lEU_vec, MD = MD_vec, C = C_vec, negpos, Vb = Vb_vec)
-df$NEUb <- 1 / 9.624643 * (df$EUb - log(df$C))
+df <- data.frame(EUb = EUb_vec, EUbtest = EUbtest_vec, NEUb = NEUb_vec, Risk = risk_vec, lNEU = lNEU_vec, MD = MD_vec, C = C_vec, negpos, Vb = Vb_vec)
+#df$NEUb <- 1 / 9.624643 * (df$EUb - log(df$C))
 ind_negw <- which(df$negpos == "negative")
 n_neg <- length(ind_negw)
 n_tot <- nrow(df)
@@ -112,32 +112,60 @@ frac_neg <- n_neg / n_tot
 print(frac_neg)
 df <- df[-ind_negw, ]
 wstars <- wstars[-ind_negw]
-ind_MD <- which(df$MD > 10)
-length(ind_MD)
-df <- df[-ind_MD, ]
-wstars <- wstars[-ind_MD]
-ind_rm <- which(df$lrat < -10)
-length(ind_rm)
-df <- df[-ind_rm, ]
-wstars <- wstars[-ind_rm]
-#------------------
+#--------------------------------------------
 ind <- which(df$NEUb == max(df$NEUb))
-data.frame(opt_NEUb = df$NEUb[ind], opt_Risk = df$Risk[ind], opt_NEUbRiskRat = df$NEUb[ind] / df$Risk[ind], opt_lNEU = df$lNEU[ind])
+opt_C <- df$C[ind]
+data.frame(opt_NEUb = df$NEUb[ind], opt_Risk = df$Risk[ind], opt_NEUbRiskRat = df$NEUb[ind] / df$Risk[ind], opt_lNEU = df$lNEU[ind], opt_C = opt_C)
 ggplot(df, aes(x = Risk, y = NEUb, color = MD)) + geom_point() + geom_vline(xintercept = df$Risk[ind])
+ggplot(df, aes(x = EUb, y = EUbtest, color = Vb)) + geom_point() + geom_vline(xintercept = df$Risk[ind])
 
-df_wStack <- data.frame(Risk = NA, EUb= NA, project = NA, w = NA)
-for(i in 1:nrow(df)){df_wStack <- rbind(df_wStack, data.frame(Risk = df$Risk[i], EUb = df$EUb[i], project = c(1:n_proj), w = c(wstars[[i]][1:n_proj])))}
-#for(i in 1:nrow(df)){df_wStack <- rbind(df_wStack, data.frame(Risk = df$Risk[i], EUb = df$EUb[i], project = c(1:n_proj), w = c(wstars[[i]][1:n_proj] / df$C[i])))}
+df_wStack <- data.frame(Risk = NA, NEUb= NA, project = NA, w = NA)
+for(i in 1:nrow(df)){df_wStack <- rbind(df_wStack, data.frame(Risk = df$Risk[i], NEUb = df$NEUb[i], project = c(1:n_proj), w = c(wstars[[i]][1:n_proj])))}
+#for(i in 1:nrow(df)){df_wStack <- rbind(df_wStack, data.frame(Risk = df$Risk[i], NEUb = df$NEUb[i], project = c(1:n_proj), w = c(wstars[[i]][1:n_proj] / df$C[i])))}
 df_wStack <- df_wStack[-1, ]
 
 df_wStack$project <- as.character(df_wStack$project)
-df_wStack$w <- df_wStack$w / 9.624643
+df_wStack$w <- df_wStack$w / df$C[ind]
 gg <- ggplot(df_wStack, aes(x = Risk, y = w)) + geom_area(aes(fill = project))
 gg <- gg + geom_vline(xintercept = df$Risk[ind])
 gg
+#============================================
+#Eigen portfolios
+#wstar_opt <- wstars[[ind]]
+#sum(wstar_opt / w_e)
+Y <- -diag(1 / s_b) %*% eigen(Corr)$vectors
+#W_e <- apply(Y, 2, function(x) x / sum(x))
+#colSums(W_e)
+#Y <- -eigen(Corr)$vectors
+W_e <- Y
+W_e <- -eigen(Corr)$vectors
+sum(t(W_e) %*% wstar_opt)
+t(Y) %*% Y
+NEUbe <- c()
+Vbe <- c()
+Ce <- c()
+for(i in 1:ncol(W_e)){
+  w_e <- W_e[, i]
+  EU_b <- as.numeric(t(1 / w_e) %*% m_b)
+  Ce[i] <- sum(w_e)
+  NEUbe[i] <- EU_b - log(Ce[i])
+  Vbe[i] <- as.numeric(t(1 / w_e) %*% S_b %*% (1 / w_e))
+}
+Risk_e <- sqrt(Vbe)
+df_e <- data.frame(Risk_e, NEUb_e = NEUbe, C_e = Ce)
+gg <- ggplot() + geom_point(data = df, aes(x = Risk, y = NEUb, color = MD))
+gg <- gg + geom_point(data = df_e, aes(x = Risk_e, y = NEUb_e))
+gg <- gg + geom_vline(xintercept = df$Risk[ind])
+gg
 
-wstar_opt <- wstars[[ind]]
-C_opt <- sum(wstar_opt)
+q <- EU_b + 1
+l_endog <- -2 * V_b / q
+invec_e <- c(w_e, l_endog)
+Vb_targ <- V_b
+rootfn <- F
+quietly <- F
+out_e <- minV_EnetUconstr(invec_e, Vb_targ, m_b, S_b, rootfn, quietly)
+
 
 
 
