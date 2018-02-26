8a7d10c3f016110a3738840a233747df4a6700a2
BenjamS <ben.schiek@gmail.com>
2018-02-25 23:42:45 -0500

    Update portfolio_optimization0218.R

--- portfolio_optimization0218.R
+++ portfolio_optimization0218.R
@@ -10,8 +10,8 @@ source('~/coloredNoise.R')
 #============================================
 #N risky assets
 #============================================
-m_b <- c(-0.37, -0.45, -0.28, -0.63)
-cv_b <- c(0.45, 0.31, 0.52, 0.23) * sign(m_b)
+m_b <- c(-0.37, -0.45, -0.28, -0.50, -0.65)
+cv_b <- c(0.35, 0.25, 0.52, 0.23, 0.15) * sign(m_b)
 s_b <- cv_b * m_b
 m_lA <- c(-.65, -0.75, -0.53, -0.3)
 cv_lA <- c(0.43, 0.81, 0.51, 0.3) * sign(m_lA)
@@ -26,17 +26,17 @@ E_lA <- sum(m_lA)
 noisevec <- coloredNoise(n_proj^2, a = -2, normalize = T, graph = T)
 Q <- matrix(exp(noisevec), n_proj, n_proj)
 Corr <- Q %*% t(Q)
-Corr <- Corr / max(Corr) * 0.1
+Corr <- Corr / max(Corr) * 0.5
 diag(Corr) <- rep(1, n_proj)
 #print(Corr)
 S_b <- diag(s_b) %*% Corr %*% diag(s_b)
 Sb_inv <- round(solve(S_b), 5)
 #============================================
-Vb_targ <- 0.9
+#Vb_targ <- 0.00592
 #--------------------------------------------
 rootfn <- T
 quietly <- F
-in_vec <- runif(n_proj + 1)
+in_vec <- runif(n_proj + 1) * 5
 out <- nleqslv(in_vec, minV_EnetUconstr, jac = NULL, Vb_targ, m_b, S_b, rootfn, quietly)
 rootfn <- F
 in_star <- out$x
@@ -46,7 +46,7 @@ Cost <- sum(wstar)
 Cost
 #============================================
 pwr10 <- 2
-range_V <- c(0, 2)
+range_V <- c(0, 1)
 first_i <- range_V[1] * 10^pwr10
 last_i <- range_V[2] * 10^pwr10
 #---------------
@@ -104,7 +104,6 @@ NEUb_vec <- EUb_vec - log(C_vec)
 EUbtest_vec <- -risk_vec * sqrt(MD_vec) + 1
 wstars <- wstar_list
 df <- data.frame(EUb = EUb_vec, EUbtest = EUbtest_vec, NEUb = NEUb_vec, Risk = risk_vec, lNEU = lNEU_vec, MD = MD_vec, C = C_vec, negpos, Vb = Vb_vec)
-#df$NEUb <- 1 / 9.624643 * (df$EUb - log(df$C))
 ind_negw <- which(df$negpos == "negative")
 n_neg <- length(ind_negw)
 n_tot <- nrow(df)
@@ -113,11 +112,17 @@ print(frac_neg)
 df <- df[-ind_negw, ]
 wstars <- wstars[-ind_negw]
 #--------------------------------------------
+ind_rm <- which(df$NEUb < -4.5)
+df <- df[-ind_rm, ]
+wstars <- wstars[-ind_rm]
+ind_dup <- which(duplicated(df$Risk))
+df <- df[-ind_dup, ]
+#--------------------------------------------
 ind <- which(df$NEUb == max(df$NEUb))
 opt_C <- df$C[ind]
 data.frame(opt_NEUb = df$NEUb[ind], opt_Risk = df$Risk[ind], opt_NEUbRiskRat = df$NEUb[ind] / df$Risk[ind], opt_lNEU = df$lNEU[ind], opt_C = opt_C)
 ggplot(df, aes(x = Risk, y = NEUb, color = MD)) + geom_point() + geom_vline(xintercept = df$Risk[ind])
-ggplot(df, aes(x = EUb, y = EUbtest, color = Vb)) + geom_point() + geom_vline(xintercept = df$Risk[ind])
+#ggplot(df, aes(x = EUb, y = EUbtest, color = Vb)) + geom_point() + geom_vline(xintercept = df$Risk[ind])
 
 df_wStack <- data.frame(Risk = NA, NEUb= NA, project = NA, w = NA)
 for(i in 1:nrow(df)){df_wStack <- rbind(df_wStack, data.frame(Risk = df$Risk[i], NEUb = df$NEUb[i], project = c(1:n_proj), w = c(wstars[[i]][1:n_proj])))}
@@ -129,6 +134,16 @@ df_wStack$w <- df_wStack$w / df$C[ind]
 gg <- ggplot(df_wStack, aes(x = Risk, y = w)) + geom_area(aes(fill = project))
 gg <- gg + geom_vline(xintercept = df$Risk[ind])
 gg
+#--------------------------------------------
+df_wStack <- data.frame(Risk = NA, NEUb= NA, project = NA, w = NA)
+#for(i in 1:nrow(df)){df_wStack <- rbind(df_wStack, data.frame(Risk = df$Risk[i], NEUb = df$NEUb[i], project = c(1:n_proj), w = c(wstars[[i]][1:n_proj])))}
+for(i in 1:nrow(df)){df_wStack <- rbind(df_wStack, data.frame(Risk = df$Risk[i], NEUb = df$NEUb[i], project = c(1:n_proj), w = c(wstars[[i]][1:n_proj] / df$C[i])))}
+df_wStack <- df_wStack[-1, ]
+df_wStack$project <- as.character(df_wStack$project)
+gg <- ggplot(df_wStack, aes(x = Risk, y = w)) + geom_area(aes(fill = project))
+gg <- gg + geom_vline(xintercept = df$Risk[ind])
+gg
+
 #============================================
 #Eigen portfolios
 #wstar_opt <- wstars[[ind]]
@@ -138,28 +153,34 @@ Y <- -diag(1 / s_b) %*% eigen(Corr)$vectors
 #colSums(W_e)
 #Y <- -eigen(Corr)$vectors
 W_e <- Y
-W_e <- -eigen(Corr)$vectors
-sum(t(W_e) %*% wstar_opt)
+#W_e <- -eigen(Corr)$vectors
+t(W_e) %*% wstar_opt
 t(Y) %*% Y
+EUbe <- c()
 NEUbe <- c()
-Vbe <- c()
+Risk_e <- c()
 Ce <- c()
 for(i in 1:ncol(W_e)){
   w_e <- W_e[, i]
   EU_b <- as.numeric(t(1 / w_e) %*% m_b)
+  EUbe[i] <- EU_b
   Ce[i] <- sum(w_e)
   NEUbe[i] <- EU_b - log(Ce[i])
-  Vbe[i] <- as.numeric(t(1 / w_e) %*% S_b %*% (1 / w_e))
+  Risk_e[i] <- sqrt(as.numeric(t(1 / w_e) %*% S_b %*% (1 / w_e)))
 }
-Risk_e <- sqrt(Vbe)
 df_e <- data.frame(Risk_e, NEUb_e = NEUbe, C_e = Ce)
+df_e <- df_e[1, ]
 gg <- ggplot() + geom_point(data = df, aes(x = Risk, y = NEUb, color = MD))
 gg <- gg + geom_point(data = df_e, aes(x = Risk_e, y = NEUb_e))
 gg <- gg + geom_vline(xintercept = df$Risk[ind])
 gg
 
+
+w_e <- W_e[, 1]
+EUb_e <- EUbe[1]
+Vb_e <- as.numeric(t(1 / w_e) %*% S_b %*% (1 / w_e))
 q <- EU_b + 1
-l_endog <- -2 * V_b / q
+l_endog <- -2 * Vb_e / q
 invec_e <- c(w_e, l_endog)
 Vb_targ <- V_b
 rootfn <- F
